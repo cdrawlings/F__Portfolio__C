b6ec64b716aafe2ca4a7a1bc376cf10e6d82f220
cdrawlings <c.d.rawlings@gmail.com>
2021-07-07 11:34:12 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,57 @@
+gsap.registerPlugin(ScrollTrigger);
+
+
+function horizontalScroll(){
+
+  const sections = gsap.utils.toArray(".panel");
+
+  gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".horizontal",
+      pin: true,
+      scrub: 1,
+      snap: 1 / (sections.length - 1),
+      // base vertical scrolling on how wide the container is so it feels more natural.
+      end: function end() {
+        return "+=" + document.querySelector(".horizontal").offsetWidth;
+      }
+    }
+  });
+}
+
+function initImageParallax(){
+  // select all sections with parallax
+  gsap.utils.toArray('.with-parallax').forEach(section => {
+
+    // get the image
+    const image = section.querySelector('img');
+
+    //creat tween
+    gsap.to(image, {
+      yPercent: 10,
+      opacity: 1,
+      ease: 'none',
+      scrollTrigger: {
+        trigger: section,
+        start: 'top center',
+        end: 'center center',
+        scrub: true
+      }
+    })
+  })
+
+}
+
+function init(){
+
+  initImageParallax();
+
+  horizontalScroll();
+
+}
+
+window.addEventListener('load', function(){
+  init();
+});
